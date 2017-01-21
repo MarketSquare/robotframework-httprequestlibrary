f3b23ff7ab19a2092276cc8bf3b1989c31e8c13d
Juho J. Saarinen <juho.saarinen@gmail.com>
2017-01-21 11:55:58 +0200

    + Added logic to handle plain JSON content correctly + Added new test case for that

--- src/main/java/com/github/hi_fi/httprequestlibrary/utils/Robot.java
+++ src/main/java/com/github/hi_fi/httprequestlibrary/utils/Robot.java
@@ -1,18 +1,17 @@
 package com.github.hi_fi.httprequestlibrary.utils;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.python.util.PythonInterpreter;
+
 import com.google.gson.Gson;
 
 public class Robot {
 
 	static RobotLogger logger = new RobotLogger("Robot");
-	
+
+	@SuppressWarnings("unchecked")
 	public static <T> T getParamsValue(String[] params, int index, T defaultValue) {
 		T value = defaultValue;
 		String givenValue = null;
@@ -36,16 +35,27 @@ public class Robot {
 	@SuppressWarnings({ "unchecked", "resource" })
 	public static Map<String, Object> parseRobotDictionary(String dictionary) {
 		logger.debug("Dictionary going to be parsed to Map: " + dictionary);
-		
+
 		PythonInterpreter py = new PythonInterpreter();
 		py.exec("import json");
-		Map<String, Object> json = new Gson().fromJson(py.eval("json.dumps("+dictionary+")").toString(), Map.class);
+		Map<String, Object> json = new Gson().fromJson(py.eval("json.dumps(" + dictionary + ")").toString(), Map.class);
 		return json;
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	public static List<String> parseRobotList(String list) {
 		logger.debug("List going to be parsed: " + list);
 		return new Gson().fromJson(list.replace("u'", "'"), List.class);
+	}
+
+	public static Boolean isDictionary(String testedString) {
+		try {
+			new Gson().fromJson(testedString, Map.class);
+			return !testedString.contains("\"");
+		} catch (Exception e) {
+			logger.debug(String.format("%s is tested for being dictionary, and result is: %s", testedString, (testedString.contains("{") && testedString.contains("}"))));
+			return (testedString.contains("{") && testedString.contains("}"));
+		}
 	}
 
 }
