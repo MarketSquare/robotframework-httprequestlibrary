edd46a7f3ce6ca6b636b262b571c64f478d2af61
Juho J. Saarinen <juho.saarinen@gmail.com>
2017-01-22 18:33:00 +0200

    + Added generic documentation about library + Removed extra HttpRequestLibrary class to prevet confusion

--- src/main/java/HttpRequestLibrary.java
+++ src/main/java/HttpRequestLibrary.java
@@ -1,7 +1,17 @@
 import org.robotframework.javalib.library.AnnotationLibrary;
 
 public class HttpRequestLibrary extends AnnotationLibrary {
-  public HttpRequestLibrary() {
-    super("com/github/hi_fi/httprequestlibrary/keywords/**");
-  }
+
+	public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
+
+	public HttpRequestLibrary() {
+		super("com/github/hi_fi/httprequestlibrary/keywords/**");
+	}
+
+	@Override
+	public String getKeywordDocumentation(String keywordName) {
+		if (keywordName.equals("__intro__"))
+			return "``HttpRequestLibrary`` is a [http://code.google.com/p/robotframework/|Robot Framework] test library that uses the [https://hc.apache.org/index.html|Apache HTTP client].";
+		return super.getKeywordDocumentation(keywordName);
+	}
 }
